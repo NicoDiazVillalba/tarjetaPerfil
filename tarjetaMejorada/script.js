12cda3b9bae88b87af555e5fecdf00f8c62753a1
NicoDiazVillalba <90287672+NicoDiazVillalba@users.noreply.github.com>
2025-03-01 08:21:12 -0500

    Alerta actualización de Página

--- tarjetaMejorada/script.js
+++ tarjetaMejorada/script.js
@@ -3,8 +3,11 @@ var boton = document.getElementById("botonCambio");
 function cambiarTexto(){
 
     var texto = document.getElementById("descripcion");
-    var nuevoTexto = "Estoy buscando oportunidades laborales que me permitan desarrollar mis habilidades en programación y bases de datos";
+    var nuevoTexto = 
+    "Estoy buscando oportunidades laborales que me permitan desarrollar-" +
+    "mis habilidades en programación y bases de datos diaznicovillalba@gmail.com - ndiazvi35180@universidadean.edu.co";
     texto.textContent = nuevoTexto;
+    alert("La información ha sido actualizada :D")
 }
 
 boton.addEventListener("click",cambiarTexto);
